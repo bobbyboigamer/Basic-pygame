18a5c862cfccfd6e58f5965bc8ac44e4a88a1908
bobbyboigamer <bobbyboigamer@gmail.com>
2023-11-11 01:06:02 -0800

    Added classes

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Hands:
+  def __init__(self, damage, speed):
+    self.damage = damage
+    self.speed = speed
+
+class User:
+  def __init__(self, speed, health):
+    self.speed = speed
+    self.health = health
