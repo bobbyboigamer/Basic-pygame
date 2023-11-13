083e508955700c80f18158cb62ad2faa5c04b9b8
bobbyboigamer <145929100+bobbyboigamer@users.noreply.github.com>
2023-11-13 10:52:29 -0800

    Removed placeholder musics

--- main.py
+++ main.py
@@ -17,21 +17,17 @@ def background_update(scrollPosition):
     window.blit(background, (0, scrollPosition))
     window.blit(background, (0, scrollPosition - HEIGHT))
     pygame.display.update()
-"""
+
 def music_update():
     music = {
         1: 'She_studious_girl_by_tohomoko.mp3',
-        2: 'Another_song.mp3',
-        3: 'Yet_another_song.mp3',
-        4: 'One_more_song.mp3',
-        5: 'Final_song.mp3',
     }
 
     for i in range(len(music)):
         pygame.mixer.music.load(music[i])
         pygame.mixer.music.play()
         pygame.time.delay(1000) 
-"""
+
 def main():
     scrollPosition = 0
     run = True
@@ -44,7 +40,7 @@ def main():
         scrollPosition %= HEIGHT + 1
 
         background_update(scrollPosition)
-#        music_update()
+        music_update()
         clock.tick(60)
 
     pygame.mixer.music.stop()
