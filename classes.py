7fb9cd1cb872742702b83f9bbc58d2404509d83b
bobbyboigamer <bobbyboigamer@gmail.com>
2023-12-24 23:28:53 -0800

    Added character movement with acceleration

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Hands:
   def __init__(self, damage, speed, length):
     self.damage = damage
@@ -5,6 +7,30 @@ class Hands:
     self.length = length
 
 class User:
-  def __init__(self, speed, health):
-    self.speed = speed
-    self.health = health
+    def __init__(self, speed, health):
+        self.speed = speed
+        self.health = health
+        self.sprite = pygame.image.load('Images/err.webp')
+        self.rect = self.sprite.get_rect()
+        self.rect.x = 207.5
+        self.rect.y = 400
+        self.acc = pygame.Vector2(0, 0)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        self.acc = pygame.Vector2(0, 0)
+
+        if pressed_keys[pygame.K_LEFT] or pressed_keys[pygame.K_a]:
+            self.acc.x = -1
+        if pressed_keys[pygame.K_RIGHT] or pressed_keys[pygame.K_d]:
+            self.acc.x = 1
+        if pressed_keys[pygame.K_UP] or pressed_keys[pygame.K_w]:
+            self.acc.y = -1
+        if pressed_keys[pygame.K_DOWN] or pressed_keys[pygame.K_s]:
+            self.acc.y = 1
+
+        self.rect.x += self.acc.x * self.speed
+        self.rect.y += self.acc.y * self.speed
+
+    def draw(self, screen):
+        screen.blit(self.sprite, self.rect)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+from pygame.locals import *
+
+from classes import *
 from musicService import startMusic
 
 WIDTH, HEIGHT = (510, 512.5)
@@ -13,16 +16,16 @@ pygame.display.set_caption("Dodge the hands!")
 background = pygame.image.load('Images/checkerboard.png')
 icon = pygame.image.load('Images/err.webp') 
 pygame.display.set_icon(icon)
+user = User(speed=2.5, health=100)
 
 def background_update(scrollPosition):
     window.blit(background, (0, scrollPosition))
     window.blit(background, (0, scrollPosition - HEIGHT))
-    pygame.display.update()
 
 def main():
     scrollPosition = 0
     run = True
-    startMusic(volume=0.5)
+    startMusic(volume=0.25)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,6 +34,9 @@ def main():
         scrollPosition = (scrollPosition + 1) % (HEIGHT + 1)
         background_update(scrollPosition)
         clock.tick(60)
+        user.update()
+        user.draw(window)
+        pygame.display.flip()
 
     pygame.mixer.music.stop()
     pygame.quit()
