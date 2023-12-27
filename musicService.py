f9763cb4b2baec64986e6e6e4b8f9e6e33b840e9
bobbyboigamer <bobbyboigamer@gmail.com>
2023-12-26 17:47:56 -0800

    Removed unnessary music

--- musicService.py
+++ musicService.py
@@ -6,8 +6,6 @@ class MusicService:
     return [
         'Music/She_studious_girl_by_tohomoko.mp3',
         'Music/2Hardlight_Groove_by_iteachvader.mp3', 
-        'Music/Wanderer_-_A_cool_vs_dave_and_bambi_agony_series_fantrack.mp3',
-        'Music/Cirno_trumpet.mp3',
         'Music/2Hardlight_Groove_by_iteachvader.mp3',
     ]
   
