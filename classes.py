98f0b6f1c558f5310b234440471a92ea6fe5cfbc
bobbyboigamer <bobbyboigamer@gmail.com>
2023-12-26 17:23:33 -0800

    Added screen boundaries for the user

--- classes.py
+++ classes.py
@@ -17,6 +17,7 @@ class User:
         self.acc = pygame.Vector2(0, 0)
 
     def update(self):
+        MAX_WIDTH, MAX_HEIGHT = 510, 512.5
         pressed_keys = pygame.key.get_pressed()
         self.acc = pygame.Vector2(0, 0)
 
@@ -31,6 +32,16 @@ class User:
 
         self.rect.x += self.acc.x * self.speed
         self.rect.y += self.acc.y * self.speed
+
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.x > MAX_WIDTH - self.rect.width:
+            self.rect.x = MAX_WIDTH - self.rect.width
+
+        if self.rect.y < 0:
+            self.rect.y = 0
+        elif self.rect.y > MAX_HEIGHT - self.rect.height:
+            self.rect.y = MAX_HEIGHT - self.rect.height
 
     def draw(self, screen):
         screen.blit(self.sprite, self.rect)
